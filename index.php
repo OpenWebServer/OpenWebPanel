7b360d15b84f71129672a2771331ee1c08669a83
shootingcharlie8 <melidosian.charlie@gmail.com>
2014-05-12 15:57:54 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,30 +1,44 @@
 <?php
 $username = "admin";
 $password = "admin";
-$randomword = "applebanannabubblechoochoo";
+$randomword = "bibblebobblechocolatemousse";
 
 if (isset($_COOKIE['MyLoginPage'])) {
    if ($_COOKIE['MyLoginPage'] == md5($password.$randomword)) {
 ?>
-
-
-
 <?php
-// start of  Control panel
+     //----------Start Of Controll Pannel----------
+?>
 
-$name = "<h1>OpenWebPanel</h1>";
-$view = "<h2>Monitor</h2>";
-$monitorram = "Monitor Ram";
-$spaceanaly = "Disk Space Ananysis";
+<!DOCTYPE html>
+ <html>
+ <head>
+         <meta charset="utf-8">
+         <title>OpenWebPanel</title>
+         <link rel="stylesheet" href="http://flip.hr/css/bootstrap.min.css">
 
-
-
-print $name;
-print $view;
-print $monitorram;
-print "</br>";
-//Start of Ram
-// get used memory
+   </head>
+ <body>
+   
+ <div id=image></div> 
+       
+ <div class="container">
+         
+         <div class="hero-unit">
+         
+<a href="settings.php" target="_blank">
+<img src="settings.png" border="2"  width="40" height="40" alt="Settings" />
+</a>
+         <h1>OpenWebPanel</h1>
+         <p>The most awesomest dev team ever!</p>
+         <!--<p><a class="btn btn-primary btn-large" href="news.html">Super important News &raquo;</a></p> -->
+         </div><!-- .hero-unit -->
+        
+ <div class="row">
+         <div class="span4">
+         <h2>Monitor Ram</h2>
+           <p>Memory used.</p>
+           <?php
 $mem_use = memory_get_usage(true) . "\n"; // 57960
 // get memory percentage
 function get_memory() {
@@ -43,11 +57,19 @@ $image2 = "&leftFill=%23CC0000";
 //percentage bar (Default)
 echo '<img src="'.$image.$tot_mem.$image2.'" alt="random image" />'."<br /><br />";
 //End of RAM
+?>
 
+           
+         <p><a class="btn" href="#">To be implamented &raquo;</a></p>
+         </div><!-- .span4 -->
+   
+         <div class="span4">
+                 <h2>Disk Space Analysis</h2>
+           <p> </p>
+           <?php
 
-
-//Start of Disk Analasis
-//Total Disk
+			//Start of Disk Analasis
+			//Total Disk
 echo "Disk Space Analasis </br>";
 $tot_disk = disk_total_space("/");
 $tot_diskad = $tot_disk/1073741824;
@@ -65,39 +87,62 @@ $free_rounded = (round($disk_gb,2));
 $disk_used = $tot_diskad - $disk_gb;
 $disk_rounded = (round($disk_used,2));
 echo $disk_rounded." GB filled of ".$tot_diskrd." GB";
-echo "</br>"
+echo "</br>";
 //Disk Left Percentage
-$percent1 = $disk_rounded / $tot_diskrd;
-$percent2 = $percent1 * 100;
-echo '<img src="'.$image.$percent2.$image2.'" alt="random image" />'."<br /><br/><br />";
+  $percent2 = $disk_rounded/$tot_diskrd;
+//print "".$disk_rounded;
+$percent3 = $percent2 * 100;
+$percent4 = (round($percent3,2));
+echo '<img src="'.$image.$percent4.$image2.'" alt="random image" />'."<br /><br />";
+  //echo (-5 % -3)."\n";         // prints -2
 
 
 
+?>
+           
+         <p><a class="btn" href="#">To be implamented &raquo;</a></p>
+         </div><!-- .span4 -->
+   
+         <div class="span4">
+                 <h2>Data Usage</h2>
+           <p>To be implamented</p>
+         <p><a class="btn" href="#">To be implamented &raquo;</a></p>
+         </div><!-- .span4 -->
+   
+ </div><!-- .row -->
+ </div><!-- .container -->
+     
+ </body>
+</html>
 
 
-// end of Control Panel
+<?php
+     
+     //----------End of Controll Panel----------
+     
 ?>
 
 
 
 
 
+
+
+
 <?php
-      exit;
+exit;
    } else {
       echo "<p>Bad cookie. Clear please clear them out and try to login again.</p>";
       exit;
    }
-   
-   
 }
 
 if (isset($_GET['p']) && $_GET['p'] == "login") {
    if ($_POST['name'] != $username) {
-      echo "<p>Sorry, that user or password does not match. Use your browser back button to go back and try again.</p>";
+      echo "<p>Sorry, that username does not match. Use your browser back button to go back and try again.</p>";
       exit;
    } else if ($_POST['pass'] != $password) {
-      echo "<p>Sorry, that user or password does not match. Use your browser back button to go back and try again.</p>";
+      echo "<p>Sorry, that password does not match. Use your browser back button to go back and try again.</p>";
       exit;
    } else if ($_POST['name'] == $username && $_POST['pass'] == $password) {
       setcookie('MyLoginPage', md5($_POST['pass'].$randomword));
@@ -105,11 +150,6 @@ if (isset($_GET['p']) && $_GET['p'] == "login") {
    } else {
       echo "<p>Sorry, you could not be logged in at this time. Refresh the page and try again.</p>";
    }
-   
-   
-   
-   
-   
 }
 ?>
 
@@ -118,8 +158,3 @@ if (isset($_GET['p']) && $_GET['p'] == "login") {
 <label><input type="password" name="pass" id="pass" /> Password</label><br />
 <input type="submit" id="submit" value="Login" />
 </fieldset></form>
-
-
-
-
-
